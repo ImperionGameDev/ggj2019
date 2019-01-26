6b7a33bebdf7417e657cc0bb3ea56ee69e8fa1cc
Gökhan Durgun <g9okhan@hotmail.com>
2019-01-26 03:49:51 +0300

    map ve game manager oluşturuldu.

---
+++ ggj/Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameStatus _status = GameStatus.MainMenu;
+    public GameStatus Status
+    {
+        get
+        {
+            return _status;
+        }
+        set
+        {
+            if (value == Status)
+            {
+                return;
+            }
+            _status = value;
+            OnStatusChanged();
+        }
+    }
+
+    public GameObject[] MainMenuObjects;
+
+    public GameObject[] GameObjects;
+
+    public GameObject[] PauseObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableDisableObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClick()
+    {
+        Status = GameStatus.Game;
+    }
+
+    private void OnStatusChanged()
+    {
+        EnableDisableObjects();
+    }
+
+    private void EnableDisableObjects()
+    {
+        bool active;
+
+        active = Status == GameStatus.MainMenu;
+        foreach (var x in MainMenuObjects)
+        {
+            x.SetActive(active);
+        }
+
+        active = Status == GameStatus.Game;
+        foreach (var x in GameObjects)
+        {
+            x.SetActive(active);
+        }
+
+        active = Status == GameStatus.Pause;
+        foreach (var x in PauseObjects)
+        {
+            x.SetActive(active);
+        }
+    }
+}
---
+++ ggj/Assets/Scripts/MapTileGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTileGenerator : MonoBehaviour
+{
+    public int mapSizeX;
+
+    public int mapSizeY;
+
+    public Sprite tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tileSize = tile.bounds.size;
+
+        for (int i = 0; i < mapSizeX; i++)
+        {
+            for (int j = 0; j < mapSizeY; j++)
+            {
+                float x = ((float)i - mapSizeX / 2f + 0.5f) * tileSize.x;
+                float y = ((float)j - mapSizeY / 2f + 0.5f) * tileSize.y;
+
+                var obj = new GameObject();
+                obj.transform.SetParent(this.transform);
+                obj.transform.position = new Vector3(x, y, -1);
+
+                var renderer = obj.AddComponent<SpriteRenderer>();
+                renderer.sprite = tile;
+                renderer.color = Color.red;
+                obj.name = $"tile({i}, {j})";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
